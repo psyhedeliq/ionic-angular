20e16158ec1714d6e1ea4c7f3a7c6114b98b0fe6
Cornel Filip <filipcornelcristian@gmail.com>
2023-05-24 16:49:04 +0300

    add about me page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'user-detail',
+    loadChildren: () => import('./user-detail/user-detail.module').then( m => m.UserDetailPageModule)
   }
 ];
 @NgModule({
